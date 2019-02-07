5934b80b5fe00ace8ce8e2ed6a9a77db4ba59962
Johannes Dillmann <johannes.dillmann@retest.de>
2019-02-07 11:20:57 +0100

    Simplify JS

--- src/main/resources/javascript/getAllElementsByPath.js
+++ src/main/resources/javascript/getAllElementsByPath.js
@@ -1,107 +1,50 @@
 cssAttributes = arguments[0];
 
-function Counter() {
-	this.map = {};
-	this.increase = function (element) {
-		if (element.tagName in this.map) {
-			this.map[element.tagName] = this.map[element.tagName] + 1;
-		} else {
-			this.map[element.tagName] = 1;
-		}
-		return this.map[element.tagName];
-	};
+function isShown(node) {
+	return !(node.offsetParent === null)
+}
+
+var result = [];
+
+var f = function(elm) {
+	for (var segs = []; elm && elm.nodeType == 1; elm = elm.parentNode)  {
+		for (i = 1, sib = elm.previousSibling; sib; sib = sib.previousSibling) { 
+			if (sib.localName == elm.localName) {
+				i++;
+			}   
+		}; 
+		segs.unshift(elm.localName.toLowerCase() + '[' + i + ']');
+	}
+	return segs.length ? '/' + segs.join('/') : null;
+}
+
+var getStyle = function(node) {
+	var result = {}
+	// var args = ["align-content", "animation-delay"]
+	var style = window.getComputedStyle(node)  || [];
+
+	args.forEach(function(e) {
+		result[e] = style[e]
+	})
+
+	return result
 }
 
 function getText(node) {
-	// 3 is text node
 	if (node.childNodes[0] && node.childNodes[0].nodeType == 3) {
 		return node.childNodes[0].nodeValue;
 	}
 	return "";
 }
 
-function getX(node) {
-	var rect = node.getBoundingClientRect();
-	return rect.left + window.scrollX;
-}
-
-function getY(node) {
-	var rect = node.getBoundingClientRect();
-	return rect.top + window.scrollY;
-}
-
-function transform(node) {
-	var extractedAttributes = {
-		"tagName": node.tagName,
-		"text": getText(node),
-		"shown": isShown(node)
-	};
-
-	// extract *all* HTML element attributes
-	var attrs = node.attributes;
-	for (var i = 0; i < attrs.length; i++) {
-		var attributeName = attrs[i].name;
-		var attributValue = attrs[i].value;
-		extractedAttributes[attributeName] = attributValue;
-	}
-
-	// extract *given* CSS style attributes
-	var style = window.getComputedStyle(node);
-	var parentStyle = [];
-	try {
-		parentStyle = window.getComputedStyle(node.parentNode);
-	} catch (err) {}
-	for (var i = 0; i < cssAttributes.length; i++) {
-		var attributeName = cssAttributes[i];
-		if (!extractedAttributes[attributeName]) {
-			if (parentStyle[attributeName] != style[attributeName]) {
-				extractedAttributes[attributeName] = style[attributeName];
-			}
-		}
-	}
-
-	// these attributes need special treatment
-	extractedAttributes["absolute-x"] = getX(node);
-	extractedAttributes["absolute-y"] = getY(node);
-	extractedAttributes["absolute-width"] = node.getBoundingClientRect().width;
-	extractedAttributes["absolute-height"] = node.getBoundingClientRect().height;
-	if (typeof node.parentNode.getBoundingClientRect === "function") {
-		extractedAttributes["x"] = getX(node) - getX(node.parentNode);
-		extractedAttributes["y"] = getY(node) - getY(node.parentNode);
-		extractedAttributes["width"] = node.getBoundingClientRect().width - node.parentNode.getBoundingClientRect().width;
-		extractedAttributes["height"] = node.getBoundingClientRect().height - node.parentNode.getBoundingClientRect().height;
-	} else {
-		extractedAttributes["x"] = getX(node);
-		extractedAttributes["y"] = getY(node);
-		extractedAttributes["width"] = node.getBoundingClientRect().width;
-		extractedAttributes["height"] = node.getBoundingClientRect().height;
-	}
-
-	return extractedAttributes;
-}
-
-function isShown(e) {
-	return !!(e.offsetWidth || e.offsetHeight || e.getClientRects().length);
-}
-
-function mapElement(element, parentPath, allElements) {
-	if (!element || !element.children) {
-		return allElements;
-	}
-	var counter = new Counter();
-	for (var i = 0; i < element.children.length; i++) {
-		var child = element.children[i];
-		var cnt = counter.increase(child);
-		var path = parentPath + "/" + child.tagName + "[" + cnt + "]";
-		allElements[path] = transform(child);
-		mapElement(child, path, allElements);
-	}
-	return allElements;
-}
-
-var htmlNode = document.getElementsByTagName("html")[0];
-var html = transform(htmlNode);
-var allElements = mapElement(htmlNode, "//HTML[1]", {
-	"//HTML[1]": html
+document.querySelectorAll('*').forEach(function(node) {
+	result.push({
+		"path" : f(node),
+		"style" : getStyle(node),
+		"tagName" : node.tagName,
+		"text" : getText(node),
+		"shown" : isShown(node)
+	})
 });
-return allElements;
+
+return JSON.stringify(result)
